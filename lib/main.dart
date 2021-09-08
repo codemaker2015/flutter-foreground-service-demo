bfa588ce8fc616ff0e9f4d19bcf60d148134f691
Vishnu Sivan <codemaker2015@gmail.com>
2021-09-08 10:39:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    startForegroundService();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Foreground Service Example'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              ElevatedButton(
+                child: Text("START"),
+                onPressed: () {
+                  startForegroundService();
+                },
+              ),
+              ElevatedButton(
+                child: Text("STOP"),
+                onPressed: () async {
+                  await FlutterForegroundPlugin.stopForegroundService();
+                },
+              ),
+              ElevatedButton(
+                child: Text("Force Exit"),
+                onPressed: () {
+                  exit(1);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void startForegroundService() async {
+  await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
+  await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
+  await FlutterForegroundPlugin.startForegroundService(
+    holdWakeLock: false,
+    onStarted: () {
+      print("Foreground on Started");
+    },
+    onStopped: () {
+      print("Foreground on Stopped");
+    },
+    title: "Flutter Foreground Service",
+    content: "Flutter Service: Hello Codemaker",
+    iconName: "ic_stat_hot_tub",
+  );
+}
+
+void globalForegroundService() {
+  debugPrint("current datetime is ${DateTime.now()}");
+}
